8da97ffaaba13a9229e9ab7f93975ef30e4ea19c
Nep T. Orshiso <nep.orshiso@gmail.com>
2021-02-06 15:56:37 -0500

    minor tweaks

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,8 @@
 $(document).ready(function () {
-
-    // Check for click events on the navbar burger icon
-    $(".navbar-burger").click(function () {
-
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
-
-    });
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function () {
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
   });
-
-  console.log("Shout out to Puja Fitzpatrick https://github.com/pujafitz for taking the time out of her day to take pictures of me at the Atlanta Tech Village. Those pictures are on all of my social media and on my portfolio. Thanks Puja!")
-
-  console.log("I'm currently looking for junior full-stack web developer roles. My email is nep.orshiso@gmail.com.")
+});
